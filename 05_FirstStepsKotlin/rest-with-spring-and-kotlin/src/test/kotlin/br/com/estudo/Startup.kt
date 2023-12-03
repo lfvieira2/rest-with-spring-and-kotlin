1b8a3977f16762ae811401e599f9edda2bec74c3
luiz <luiz>
2023-12-03 16:30:09 -0300

    Primeiros passos com kotlin

---
+++ 05_FirstStepsKotlin/rest-with-spring-and-kotlin/src/test/kotlin/br/com/estudo/Startup.kt
@@ -0,0 +1,12 @@
+package br.com.estudo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class StartupTest {
+
+	@Test
+	fun contextLoads() {
+	}
+}
